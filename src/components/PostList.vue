e1e6bf64a376900d6d7f2f7358bd10a5a1a81aa6
Alexandre <alexandre.vauthier@edu.devinci.fr>
2021-03-21 15:07:00 +0100

    Blog style

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,22 +1,39 @@
-<template>
-      <table>
-            <div v-for="post in posts" v-bind:key="post">
-                  <thead>
-                        <tr>
-                              <th colspan="2">{{ post.title }}</th>
-                        </tr>
-                  </thead>
-                  <tbody>
-                        <tr>
-                              <!-- <td>{{ post.metaTitle }}</td>
-                              <td>{{ post.metaDescription }}</td> -->
-                              <td>{{ post.imageUrl }}</td>
-                              <td>{{ post.content }}</td>
-                        </tr>
-                  </tbody>
-            </div>
+<style>
+      .post{
+            margin: auto;
+            border: 2px solid black;
+            margin-top: 15px;
+            width: 950px;
+      }
+      .container{
+            margin: auto;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 300px;
+            width: 900px;
+      }
+      .container img{
+            width: 350px;
+            height: 231px;
+      }
+      .container p{
+            margin: 0px 10px;
+            width: 580px;
+      }
+</style>
 
-      </table>
+<template>
+      <div>
+            <div class="post" v-for="post in posts" v-bind:key="post" >
+                  <h2>{{post.title}}</h2>
+                  <div class="container">
+                        <img :src="post.imageUrl" alt="lorem picsum">
+                        <p>{{post.content}}</p>
+                  </div>
+            </div>
+      </div>
+
 </template>
 
 <script>
